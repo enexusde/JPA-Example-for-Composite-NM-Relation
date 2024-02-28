13ed210b7894e78b98edba8bf74f0ddbf5a71ec0
Peter Rader <github@e-nexus.de>
2024-02-28 03:49:10 +0100

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO Teacher VALUES (YEAR(NOW()) - 1881, 'Albus', 'Dumbledore');
+INSERT INTO Teacher VALUES (YEAR(NOW()) - 1956, 'Argus', 'Filch');
+INSERT INTO Student VALUES (YEAR(NOW()) - 1980, 'Harry', 'Potter');
+INSERT INTO Student VALUES (YEAR(NOW()) - 1980, 'Ronald', 'Weasley');
+INSERT INTO Teacher2Student VALUES ('Harry', 'Potter', 'Albus', 'Dumbledore');
+INSERT INTO Teacher2Student VALUES ('Ronald', 'Weasley', 'Argus', 'Filch');
