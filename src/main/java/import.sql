a9e6b51712da73049d8c3be003664f87b3c68a86
Peter Rader <github@e-nexus.de>
2024-02-28 03:49:37 +0100

    Delete src/main/java/import.sql

--- src/main/java/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Teacher VALUES (YEAR(NOW()) - 1881, 'Albus', 'Dumbledore');
-INSERT INTO Teacher VALUES (YEAR(NOW()) - 1956, 'Argus', 'Filch');
-INSERT INTO Student VALUES (YEAR(NOW()) - 1980, 'Harry', 'Potter');
-INSERT INTO Student VALUES (YEAR(NOW()) - 1980, 'Ronald', 'Weasley');
-INSERT INTO Teacher2Student VALUES ('Harry', 'Potter', 'Albus', 'Dumbledore');
-INSERT INTO Teacher2Student VALUES ('Ronald', 'Weasley', 'Argus', 'Filch');
